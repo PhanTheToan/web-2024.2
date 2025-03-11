a17b98992a6662c0d348004cc68d517b33b5675b
PhanTheToan <ptt.hust.dev@gmail.com>
2025-03-11 14:24:26 +0700

    config-env

--- backend/src/main/java/web20242/webcourse/WebcourseApplication.java
+++ backend/src/main/java/web20242/webcourse/WebcourseApplication.java
@@ -1,5 +1,6 @@
 package web20242.webcourse;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebcourseApplication {
 
 	public static void main(String[] args) {
+		// Config dotenv
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("DATA_BASE_USER", dotenv.get("DATA_BASE_USER"));
+		System.setProperty("DATA_BASE_PASSWORD", dotenv.get("DATA_BASE_PASSWORD"));
+		System.setProperty("DATA_BASE_NAME", dotenv.get("DATA_BASE_NAME"));
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
+
 		SpringApplication.run(WebcourseApplication.class, args);
+
 	}
 
 }
