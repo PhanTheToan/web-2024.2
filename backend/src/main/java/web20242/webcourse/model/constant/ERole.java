3b82159859f1811a4088c05064dd92afac96f479
PhanTheToan <ptt.hust.dev@gmail.com>
2025-04-22 17:19:35 +0700

    delete/infor/backend

--- backend/src/main/java/web20242/webcourse/model/constant/ERole.java
+++
@@ -1,7 +0,0 @@
-package web20242.webcourse.model.constant;
-
-public enum ERole {
-    ROLE_USER,
-    ROLE_TEACHER,
-    ROLE_ADMIN,
-}
--- backend/src/main/java/web20242/webcourse/model/constant/EStatus.java
+++
@@ -1,9 +0,0 @@
-package web20242.webcourse.model.constant;
-
-public enum EStatus {
-    DONE,
-    INPROGRESS,
-    NOTSTARTED,
-    ACTIVE,
-    INACTIVE,
-}
