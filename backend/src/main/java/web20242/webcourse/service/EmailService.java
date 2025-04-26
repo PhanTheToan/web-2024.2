5a1febe2616e60bc11f2e17cdd438f067fa8b91b
PhanTheToan <phantoan3009@gmail.com>
2025-04-26 22:43:57 +0700

    fixed/l1

--- backend/src/main/java/web20242/webcourse/service/EmailService.java
+++ backend/src/main/java/web20242/webcourse/service/EmailService.java
@@ -3,11 +3,14 @@ package web20242.webcourse.service;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StreamUtils;
 
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -21,9 +24,9 @@ public class EmailService {
         MimeMessage message = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true, StandardCharsets.UTF_8.name());
 
-        try {
-            String htmlTemplate = new String(Files.readAllBytes(Paths.get("src/main/java/web20242/webcourse/model/template/mailOtp.hbs")), StandardCharsets.UTF_8);
-
+        String htmlTemplate = null; // Khởi tạo giá trị mặc định hoặc null
+        try (InputStream inputStream = new ClassPathResource("templates/mailOtp.hbs").getInputStream()) {
+            htmlTemplate = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
             htmlTemplate = htmlTemplate.replace("{{fullName}}", username);
             htmlTemplate = htmlTemplate.replace("{{otpCode}}", otp);
             htmlTemplate = htmlTemplate.replace("{{recipientEmail}}", email);
@@ -38,6 +41,7 @@ public class EmailService {
             return ResponseEntity.ok("Send email success fully");
 
         } catch (Exception e) {
+
             throw new MessagingException("Failed to send email: " + e.getMessage(), e);
         }
 
