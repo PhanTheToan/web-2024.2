7081bc0bff85ae9b1830622205581f46878ac9a2
PhanTheToan <phantoan3009@gmail.com>
2025-04-27 02:17:38 +0700

    refactor/send-email

--- backend/src/main/java/web20242/webcourse/controller/EmailController.java
+++ backend/src/main/java/web20242/webcourse/controller/EmailController.java
@@ -3,15 +3,19 @@ package web20242.webcourse.controller;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.core.parameters.P;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import web20242.webcourse.model.createRequest.EmailDetail;
 
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -27,8 +31,9 @@ public class EmailController {
         MimeMessage message = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true, StandardCharsets.UTF_8.name());
 
-        try {
-            String htmlTemplate = new String(Files.readAllBytes(Paths.get("src/main/java/web20242/webcourse/model/template/thanks.hbs")), StandardCharsets.UTF_8);
+        String htmlTemplate = null;
+        try (InputStream inputStream = new ClassPathResource("templates/thanks.hbs").getInputStream()) {
+            htmlTemplate = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
 
             htmlTemplate = htmlTemplate.replace("{{fullName}}", emailDetail.getFullName());
             htmlTemplate = htmlTemplate.replace("{{message}}", emailDetail.getMessage());
@@ -39,6 +44,31 @@ public class EmailController {
             helper.setText(htmlTemplate, true);
             helper.setFrom("mclasspart1@gmail.com");
 
+            javaMailSender.send(message);
+
+        } catch (Exception e) {
+            throw new MessagingException("Failed to send email: " + e.getMessage(), e);
+        }
+    }
+    @PostMapping("/contact")
+    public void sendEmailContactToAdmin(@RequestBody EmailDetail emailDetail) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, StandardCharsets.UTF_8.name());
+
+        String htmlTemplate = null;
+        try (InputStream inputStream = new ClassPathResource("templates/contact.hbs").getInputStream()) {
+            htmlTemplate = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+            htmlTemplate = htmlTemplate.replace("{{fullName}}", emailDetail.getFullName());
+            htmlTemplate = htmlTemplate.replace("{{message}}", emailDetail.getMessage());
+            htmlTemplate = htmlTemplate.replace("{{recipientEmail}}", emailDetail.getEmail());
+
+            helper.setTo("mclasspart1@gmail.com");
+            helper.setSubject(emailDetail.getFullName()+" từ ContactForm");
+            helper.setText(htmlTemplate, true);
+            helper.setFrom(emailDetail.getEmail());
+
+            sendEmailForThanksUser(emailDetail);
             javaMailSender.send(message);
 
         } catch (Exception e) {
